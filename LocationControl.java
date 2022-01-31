19cc56b4f11e64f3cb7ca568cabd5d757124f537
cgok <44663880+cgok@users.noreply.github.com>
2022-02-01 00:01:36 +0300

    Update LocationControl.java

--- LocationControl.java
+++ LocationControl.java
@@ -1,13 +1,5 @@
 public interface LocationControl {
 	
-	/*
-	 * 
-	 * Name, Surname: Can Gok
-	 * 
-	 * Student number: 150118014
-	 * 
-	 */
-	
 	//onLeave and onCome methods explained in the class.
 	
 	public abstract void onLeave();
