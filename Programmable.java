c23acedc7919f3d2043ca854e2efce4d94e374d9
cgok <44663880+cgok@users.noreply.github.com>
2022-02-01 00:01:12 +0300

    Update Programmable.java

--- Programmable.java
+++ Programmable.java
@@ -1,13 +1,5 @@
 public interface Programmable {
 	
-	/*
-	 * 
-	 * Name, Surname: Can Gok
-	 * 
-	 * Student Number: 150118014
-	 * 
-	 */
-	
 	//setTimer, cancelTimer and runProgram methods explained in the class.
 	
 	public abstract void setTimer(int seconds);
